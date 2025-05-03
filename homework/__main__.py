dc449c8d3da660a5c42c9ff167d8ea6947ac233c
Esteban Gómez Benítez <esgomez@unal.edu.co>
2025-05-03 11:59:41 -0500

    Taller completado

---
+++ homework/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for the homework module."""
+
+from homework.src.wordcount import main
+
+if __name__ == "__main__":
+    main()
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,5 +1,6 @@
 # obtain a list of files in the input directory
 import os
+import sys
 
 from homework.src._internals.count_words import count_words
 from homework.src._internals.preprocess_lines import preprocess_lines
@@ -10,7 +11,14 @@ from homework.src._internals.write_word_counts import write_word_counts
 
 def main():
 
-    all_lines = read_all_lines()
+    if len(sys.argv) != 3:
+        print("Usage: python wordcount.py <input_folder> <output_folder>")
+        sys.exit(1)
+
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    all_lines = read_all_lines(input_folder)
 
     all_lines = preprocess_lines(all_lines)
 
@@ -18,7 +26,7 @@ def main():
 
     counter = count_words(words)
 
-    write_word_counts(counter)
+    write_word_counts(counter, output_folder)
 
 
 if __name__ == "__main__":
